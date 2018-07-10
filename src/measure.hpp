8544fe8f398da489054fca9fed2f979622cc2f35
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-10 22:04:31 +0200

    Add buffer for storing measurment results

--- src/measure.hpp
+++ src/measure.hpp
@@ -1,7 +1,13 @@
 #ifndef MEASURE_HPP
 #define MEASURE_HPP
 
+template <uint16_t BufferSize>
 class Measure {
+ public:
+
+ private:
+  uint16_t last_value = 0;
+  uint8_t buffer[BufferSize];
 };
 
 #endif  // MEASURE_HPP
