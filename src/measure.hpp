1a6551125b5047f4d4f8b08b5b16a3c5d0cfc36c
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-10 22:07:10 +0200

    Add function to send gathered data

--- src/measure.hpp
+++ src/measure.hpp
@@ -1,9 +1,20 @@
 #ifndef MEASURE_HPP
 #define MEASURE_HPP
 
+#include "adc.hpp"
+#include "usart.hpp"
+
 template <uint16_t BufferSize>
 class Measure {
  public:
+  void flush_data_via_USART(Usart& usart) {
+    for (; last_value == 0 || last_value % BufferSize; ++last_value) {
+      usart.send_byte(buffer[last_value]);
+    }
+
+    last_value = 0;
+  }
+
   inline bool is_full() const { return last_value + 1 == BufferSize; }
 
   inline void store_measured_data(const uint8_t data) { buffer[last_value++]; }
@@ -11,6 +22,7 @@ class Measure {
  private:
   uint16_t last_value = 0;
   uint8_t buffer[BufferSize];
+  const Adc<> adc;
 };
 
 #endif  // MEASURE_HPP
