5908780e424c8c4d24f50a5df531c7d304480942
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-10 22:12:41 +0200

    Add docs to Measure class

--- src/measure.hpp
+++ src/measure.hpp
@@ -7,6 +7,11 @@
 template <uint16_t BufferSize>
 class Measure {
  public:
+  /**
+   * @brief Flushes buffer to USART buffer, queueing it to be sent
+   *
+   * @param usart an Usart reference
+   */
   void flush_data_via_USART(Usart& usart) {
     for (; last_value == 0 || last_value % BufferSize; ++last_value) {
       usart.send_byte(buffer[last_value]);
@@ -15,8 +20,19 @@ class Measure {
     last_value = 0;
   }
 
+  /**
+   * @brief Checks if internal buffer is full
+   *
+   * @return true
+   * @return false
+   */
   inline bool is_full() const { return last_value + 1 == BufferSize; }
 
+  /**
+   * @brief Stores provided data in buffer
+   *
+   * @param data
+   */
   inline void store_measured_data(const uint8_t data) { buffer[last_value++]; }
 
  private:
