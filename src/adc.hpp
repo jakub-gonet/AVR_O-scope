ba91cc3790a8b81836a8882e7ff85292ff7672c6
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-10 19:27:12 +0200

    Implement c-tor using Avr class

--- src/adc.hpp
+++ src/adc.hpp
@@ -4,8 +4,22 @@
 extern "C" {
 #include <avr/io.h>
 }
+#include "util/avr.hpp"
 
+template <class HW = Avr>
 class Adc {
+ public:
+  inline Adc() {
+    // Uref = AVCC with external capacitor at AREF pin,
+    // set let adjust of result for convienient 8bit access
+    HW::set(ADMUX, (1 << REFS0) | (1 << ADLAR));
+
+    // ADC enable, free running mode, start first
+    // conversion, interrupt on update, turn on
+    // interrupts default prescaler (/2)
+    HW::set(ADCSRA, (1 << ADEN) | (1 << ADFR) | (1 << ADSC) | (1 << ADIF) |
+                        (1 << ADIE));
+  }
 };
 
 #endif  // ADC_HPP
