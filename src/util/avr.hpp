f69df5e052f9180f82094e5bb33bdb094286ddc2
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-12 12:23:35 +0200

    Add a bunch of registers and bit defs as functions and class members
    Not all obviously, I'll be adding them when needed.

--- src/util/avr.hpp
+++ src/util/avr.hpp
@@ -32,6 +32,19 @@ class Avr {
     return address & ~value;
   }
 
+  // members
+
+  constexpr static volatile uint8_t& Admux() { return ADMUX; }
+  constexpr static volatile uint8_t& Adcsra() { return ADCSRA; }
+  constexpr static volatile uint8_t& Adch() { return ADCH; }
+  constexpr static uint8_t Refs0 = (1 << REFS0);
+  constexpr static uint8_t Adlar = (1 << ADLAR);
+  constexpr static uint8_t Aden = (1 << ADEN);
+  constexpr static uint8_t Adfr = (1 << ADFR);
+  constexpr static uint8_t Adsc = (1 << ADSC);
+  constexpr static uint8_t Adif = (1 << ADIF);
+  constexpr static uint8_t Adie = (1 << ADIE);
+
  private:
   Avr() {}
   ~Avr() {}
