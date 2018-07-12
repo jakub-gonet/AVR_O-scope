2985fbc81dbd3882998abf35a6efab21728d9aaf
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-12 15:50:10 +0200

    Add defines for USART

--- src/util/avr.hpp
+++ src/util/avr.hpp
@@ -30,9 +30,11 @@ class Avr {
 
   // members
 
+  // ADC
   constexpr static volatile uint8_t& Admux() { return ADMUX; }
   constexpr static volatile uint8_t& Adcsra() { return ADCSRA; }
   constexpr static volatile uint8_t& Adch() { return ADCH; }
+
   constexpr static uint8_t Refs0 = (1 << REFS0);
   constexpr static uint8_t Adlar = (1 << ADLAR);
   constexpr static uint8_t Aden = (1 << ADEN);
@@ -40,6 +42,23 @@ class Avr {
   constexpr static uint8_t Adsc = (1 << ADSC);
   constexpr static uint8_t Adif = (1 << ADIF);
   constexpr static uint8_t Adie = (1 << ADIE);
+
+  // USART
+  constexpr static volatile uint8_t& Ubrrh() { return UBRRH; }
+  constexpr static volatile uint8_t& Ubrrl() { return UBRRL; }
+  constexpr static volatile uint8_t& Ucsra() { return UCSRA; }
+  constexpr static volatile uint8_t& Ucsrb() { return UCSRB; }
+  constexpr static volatile uint8_t& Ucsrc() { return UCSRC; }
+  constexpr static volatile uint8_t& Udr() { return UDR; }
+
+  constexpr static uint8_t Rxcie = (1 << RXCIE);
+  constexpr static uint8_t Txen = (1 << TXEN);
+  constexpr static uint8_t Rxen = (1 << RXEN);
+  constexpr static uint8_t Ursel = (1 << URSEL);
+  constexpr static uint8_t Ucsz0 = (1 << UCSZ0);
+  constexpr static uint8_t Ucsz1 = (1 << UCSZ1);
+  constexpr static uint8_t Udrie = (1 << UDRIE);
+  constexpr static uint8_t Udre = (1 << UDRE);
 
  private:
   Avr() {}
