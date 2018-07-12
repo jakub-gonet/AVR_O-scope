6a7cecb4a5f765ee4c6fa66326eb64e8fe5d4a55
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-12 12:25:32 +0200

    Make ADC use functions from HAL class (util/avr.hpp)

--- src/adc.hpp
+++ src/adc.hpp
@@ -1,30 +1,26 @@
 #ifndef ADC_HPP
 #define ADC_HPP
 
-extern "C" {
-#include <avr/io.h>
-}
-#include "util/avr.hpp"
-
-template <class HW = Avr>
+template <class HW>
 class Adc {
  public:
   inline Adc() {
     // Uref = AVCC with external capacitor at AREF pin,
     // set let adjust of result for convienient 8bit access
-    HW::set(ADMUX, (1 << REFS0) | (1 << ADLAR));
+    HW::set(HW::Admux(), HW::Refs0 | HW::Adlar);
 
     // ADC enable, free running mode, start first
     // conversion, interrupt on update, turn on
     // interrupts default prescaler (/2)
-    HW::set(ADCSRA, (1 << ADEN) | (1 << ADFR) | (1 << ADSC) | (1 << ADIF) |
-                        (1 << ADIE));
+    HW::set(HW::Adcsra(), HW::Aden | HW::Adfr | HW::Adsc | HW::Adif | HW::Adie);
   }
 
-  inline uint8_t get_8bit_conversion_result() const { return HW::get(ADCH); }
+  inline uint8_t get_8bit_conversion_result() const {
+    return HW::get(HW::Adch);
+  }
 
-  inline void stop_ADC() const { HW::clear(ADCSRA, (1 << ADEN)); }
-  inline void start_ADC() const { HW::update(ADCSRA, (1 << ADEN)); }
+  inline void stop_ADC() const { HW::clear(HW::Adcsra(), HW::Aden); }
+  inline void start_ADC() const { HW::update(HW::Adcsra(), HW::Aden); }
 };
 
 #endif  // ADC_HPP
