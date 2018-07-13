ac97453d5c025064a3ab25484fc38200a9b7fea4
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-13 14:36:48 +0200

    Add docs to Measure

--- src/measure.hpp
+++ src/measure.hpp
@@ -14,11 +14,11 @@ class Measure {
    * @param usart an Usart reference
    */
   void flush_data_via_USART(Usart<HW>& usart) {
-    disableAdc();
+    disable_measurements();
     while (!buffer.is_empty()) {
       usart.send_byte(buffer.get());
     }
-    enableAdc();
+    enable_measurements();
   }
 
   /**
@@ -30,23 +30,36 @@ class Measure {
   inline bool is_buffer_full() const { return buffer.is_full(); }
 
   /**
+   * @brief Checks if internal buffer is empty
+   *
+   * @return true
+   * @return false
+   */
+  inline bool is_buffer_empty() const { return buffer.is_empty(); }
+
+  /**
    * @brief Stores provided data in buffer
    *
    * @param data
    */
-  inline bool store_measured_data(const uint8_t data) {
-    return buffer.put(data);
+  inline bool store_measured_data() {
+    return buffer.put(adc.get_8bit_conversion_result());
   }
 
-  inline uint8_t get_ADC_result() const {
-    return adc.get_8bit_conversion_result();
-  }
+  /**
+   * @brief Disables ADC
+   *
+   */
+  inline void disable_measurements() const { adc.stop_ADC(); }
 
-  inline void disableAdc() const { adc.stop_ADC(); }
-  inline void enableAdc() const { adc.start_ADC(); }
+  /**
+   * @brief Enables ADC
+   *
+   */
+  inline void enable_measurements() const { adc.start_ADC(); }
 
  private:
-  FifoQueue<BufferSize> buffer;
+  FifoQueue<uint16_t, BufferSize> buffer;
   const Adc<HW> adc;
 };
 
