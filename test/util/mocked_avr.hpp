eff77feec9c1cbdc0bebc741030a2fb49868e4e9
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-20 09:47:10 +0200

    Add new values to MockerAvr

--- test/util/mocked_avr.hpp
+++ test/util/mocked_avr.hpp
@@ -8,28 +8,55 @@ extern "C" {
 
 class MockedAvr {
  public:
-  static inline volatile uint8_t& get(volatile uint8_t& address) { return address; }
+  static inline volatile uint8_t& get(volatile uint8_t& address) {
+    return address;
+  }
 
-  static inline void set(volatile uint8_t& address, const uint8_t value) { address = value; }
+  static inline void set(volatile uint8_t& address, const uint8_t value) {
+    address = value;
+  }
 
-  static inline void clear(volatile uint8_t& address, const uint8_t value) { address &= ~value; }
+  static inline void clear(volatile uint8_t& address, const uint8_t value) {
+    address &= ~value;
+  }
 
-  static inline void update(volatile uint8_t& address, const uint8_t value) { address |= value; }
+  static inline void update(volatile uint8_t& address, const uint8_t value) {
+    address |= value;
+  }
 
   static inline bool is_set(volatile uint8_t& address, const uint8_t value) {
     return address & value;
   }
 
-  static inline bool is_cleared(volatile uint8_t& address, const uint8_t value) {
+  static inline bool is_cleared(volatile uint8_t& address,
+                                const uint8_t value) {
     return address & ~value;
   }
 
   // members
 
+  // IO
+
+  static volatile uint8_t& DdrC() { return registers.DdrC; }
+
+  constexpr static uint8_t Pc0 = (1 << 0);
+  constexpr static uint8_t Pc1 = (1 << 1);
+  constexpr static uint8_t Pc2 = (1 << 2);
+  constexpr static uint8_t Pc3 = (1 << 3);
+  constexpr static uint8_t Pc4 = (1 << 4);
+  constexpr static uint8_t Pc5 = (1 << 5);
+  constexpr static uint8_t Pc6 = (1 << 6);
+
+  // ADC
+
   static volatile uint8_t& Admux() { return registers.Admux; }
   static volatile uint8_t& Adcsra() { return registers.Adcsra; }
   static volatile uint8_t& Adch() { return (registers.Adch); }
 
+  constexpr static uint8_t Mux0 = (1 << 0);
+  constexpr static uint8_t Mux1 = (1 << 1);
+  constexpr static uint8_t Mux2 = (1 << 2);
+  constexpr static uint8_t Mux3 = (1 << 3);
   constexpr static uint8_t Adps2 = (1 << 2);
   constexpr static uint8_t Adps1 = (1 << 1);
   constexpr static uint8_t Adps0 = (1 << 0);
--- test/util/mocked_avr_registers.hpp
+++ test/util/mocked_avr_registers.hpp
@@ -10,6 +10,7 @@ class MockedAvrRegisters {
   uint8_t Admux = 0;
   uint8_t Adcsra = 0;
   uint8_t Adch = 0;
+  uint8_t DdrC = 0;
 };
 
 #endif  // MOCKED_AVR_REGISTERS_HPP
